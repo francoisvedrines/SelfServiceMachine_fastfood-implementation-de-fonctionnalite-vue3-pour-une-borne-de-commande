26bfe849450788869767f10de7a71729c327abc5
francoisvedrines <f.vedrines@netc.fr>
2023-05-11 18:19:10 +0200

    gestion evenements, liaison attribut, methode

--- scripts.js
+++ scripts.js
@@ -70,9 +70,19 @@ const SelfServiceMachine = {
         return {
             products: window.products
         }
+    },
+    methods: {
+        total: function() {
+            let total = 0;
+            this.products.forEach(function(item){
+                if (item.active){
+                    total += item.price * item.quantity;
+                }
+            });
+            return total.toFixed(2);
+        }
     }
-
-}
+};
 
 
 
